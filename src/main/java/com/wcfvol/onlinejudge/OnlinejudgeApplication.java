df79d1aec628ae9c7c927f34e5df04bc6152ca5e
Wang Chunfei <1041875526@qq.com>
2018-06-22 19:09:37 +0800

    init commit

---
+++ src/main/java/com/wcfvol/onlinejudge/OnlinejudgeApplication.java
@@ -0,0 +1,12 @@
+package com.wcfvol.onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinejudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinejudgeApplication.class, args);
+    }
+}
