0859d1b9da0399e038a871a560c4bab80abf2010
Wang Chunfei <1041875526@qq.com>
2018-07-01 16:08:45 +0800

    add admin/ResultController

---
+++ src/main/java/com/wcfvol/onlinejudge/controller/admin/ResultController.java
@@ -0,0 +1,37 @@
+package com.wcfvol.onlinejudge.controller.admin;
+
+import com.alibaba.fastjson.JSONObject;
+import com.wcfvol.onlinejudge.entity.Submission;
+import com.wcfvol.onlinejudge.service.SubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @ClassName ResultController
+ * @Author Wang Chunfei
+ * @Date 2018/7/1 下午4:00
+ **/
+@Controller
+@RequestMapping(value = "/admin")
+public class ResultController {
+    @Autowired
+    SubmissionService submissionService;
+
+    @RequestMapping(value = "/set_result",method = RequestMethod.POST)
+    @ResponseBody
+    public String setResult(@RequestBody String body) {
+        JSONObject jsonResult = new JSONObject();
+        JSONObject jsonBody = (JSONObject) JSONObject.parse(body);
+        Submission submission = new Submission();
+        submission.setId(jsonBody.getInteger("id"));
+        submission.setLength(jsonBody.getInteger("length"));
+        submission.setMemory(jsonBody.getInteger("memory"));
+        submission.setTime(jsonBody.getDouble("time"));
+        submission.setResult(jsonBody.getInteger("result"));
+        submissionService.updateResult(submission);
+        jsonResult.put("ok",1);
+        return jsonResult.toJSONString();
+    }
+
+}
