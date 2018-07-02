a8bd113c0edc38fd952ec64a8c785a410c0e6647
Wang Chunfei <1041875526@qq.com>
2018-07-02 10:28:57 +0800

    addProblem controller

--- src/main/java/com/wcfvol/onlinejudge/controller/admin/ResultController.java
+++ src/main/java/com/wcfvol/onlinejudge/controller/admin/ResultController.java
@@ -1,11 +1,18 @@
 package com.wcfvol.onlinejudge.controller.admin;
 
 import com.alibaba.fastjson.JSONObject;
+import com.wcfvol.onlinejudge.entity.Problem;
+import com.wcfvol.onlinejudge.entity.ProblemList;
 import com.wcfvol.onlinejudge.entity.Submission;
 import com.wcfvol.onlinejudge.po.RestResult;
+import com.wcfvol.onlinejudge.service.ProblemListService;
+import com.wcfvol.onlinejudge.service.ProblemService;
 import com.wcfvol.onlinejudge.service.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 /**
  * @ClassName ResultController
@@ -17,6 +24,11 @@ import org.springframework.web.bind.annotation.*;
 public class ResultController {
     @Autowired
     SubmissionService submissionService;
+    @Autowired
+    ProblemService problemService;
+    @Autowired
+    ProblemListService problemListService;
+
 
     @RequestMapping(value = "/set_result",method = RequestMethod.POST)
     public RestResult setResult(@RequestBody String body) {
@@ -31,5 +43,32 @@ public class ResultController {
         return RestResult.ok();
     }
 
+    @RequestMapping(value = "/add_problem",method = RequestMethod.POST)
+    @Transactional(rollbackFor = Exception.class)
+    public RestResult addProblem(@RequestBody String body) {
+        JSONObject jsonBody = (JSONObject) JSONObject.parse(body);
+        Problem problem = new Problem();
+        problem.setDescription(jsonBody.getString("description"));
+        problem.setInput(jsonBody.getString("input"));
+        problem.setOutput(jsonBody.getString("output"));
+        problem.setTitle(jsonBody.getString("title"));
+        problem.setHint(jsonBody.getString("hint"));
+        problem.setSampleInput(jsonBody.getString("sampleInput"));
+        problem.setSampleOutput(jsonBody.getString("sampleOutput"));
+        ProblemList problemList = new ProblemList();
+        problemList.setAuthor(jsonBody.getString("author"));
+        problemList.setSource(jsonBody.getString("source"));
+        problemList.setTitle(jsonBody.getString("title"));
+        problemService.addProblem(problem);
+        problemList.setProblemId(problem.getId());
+        problemListService.addProblemList(problemList);
+        return RestResult.ok();
+    }
+
+    @RequestMapping(value = "/add_input_output",method = RequestMethod.POST)
+    public RestResult addInputOutput(@RequestParam("input")File input, @RequestParam("output") File output) {
+        // TODO: 2018/7/2  
+        return RestResult.ok();
+    }
 
 }
